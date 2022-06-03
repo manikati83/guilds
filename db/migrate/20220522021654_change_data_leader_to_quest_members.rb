2e8e24f027ceac61f45829d75a3d8ef5190233a7
Ubuntu <ubuntu@ip-172-31-95-30.ec2.internal>
2022-06-03 10:22:47 +0000

    change column

--- db/migrate/20220522021654_change_data_leader_to_quest_members.rb
+++ db/migrate/20220522021654_change_data_leader_to_quest_members.rb
@@ -1,5 +1,6 @@
 class ChangeDataLeaderToQuestMembers < ActiveRecord::Migration[6.0]
   def change
-    change_column :quest_members, :leader_id, :boolean, default: false
+    #change_column :quest_members, :leader_id, :boolean, default: false
+    change_column :quest_members, :leader_id, 'boolean USING CAST(leader_id AS boolean)'
   end
 end
